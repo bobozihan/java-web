dd285da95398b6a423da143544df570b6798bb48
hualuomoli <liubaoquan0831@163.com>
2016-01-05 11:25:22 +0800

    add

--- bin/install.bat
+++ bin/install.bat
@@ -22,6 +22,10 @@ echo Install Commons
 cd %basePath%/modules/commons
 call mvn clean install -Dmaven.test.skip=true
 
+echo Install Spring
+cd %basePath%/modules/spring
+call mvn clean install -Dmaven.test.skip=true
+
 echo Install Base
 cd %basePath%/modules/base
 call mvn clean install -Dmaven.test.skip=true
