f7e19863349df2b81be5558e18e01793e8b92404
hualuomoli <liubaoquan0831@163.com>
2016-01-05 11:46:09 +0800

    add

---
+++ modules/core/src/main/java/com/github/hualuomoli/factory/config/PropertyPlaceholderConfigurer.java
@@ -0,0 +1,10 @@
+package com.github.hualuomoli.factory.config;
+
+/**
+ * extend spring PropertyPlaceholderConfigurer
+ * @author hualuomoli
+ *
+ */
+public class PropertyPlaceholderConfigurer extends org.springframework.beans.factory.config.PropertyPlaceholderConfigurer {
+
+}
